cd1148f381659c62b1e43a14d1c1aa188ab315a4
Ulkeshwani <sureshwani30@gmail.com>
2022-04-04 23:30:19 +0530

    Add: Added New Popup Model 🐱‍🚀

---
+++ src/Components/Custom Model/Model.component.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  DialogContentText,
+  Button,
+  Box,
+  Typography,
+} from "@mui/material";
+
+interface ModelProps {
+  children?: React.ReactChild;
+  open: boolean;
+  fullScreen?: boolean;
+  title?: string;
+  message?: string;
+  handleDialog: () => void;
+}
+
+const Model = (props: ModelProps) => {
+  const { children } = props;
+  return (
+    <Dialog
+      open={props.open}
+      onClose={props.handleDialog}
+      fullScreen={props?.fullScreen}
+      maxWidth="md"
+      fullWidth
+    >
+      <DialogTitle color="primary">{props?.title}</DialogTitle>
+      <DialogContent dividers={true}>
+        <DialogContentText>{props?.message}</DialogContentText>
+        <Box sx={{ p: 1 }}>
+          {!children ? (
+            <Typography variant="body2">No Data Found</Typography>
+          ) : (
+            children
+          )}
+        </Box>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default Model;
