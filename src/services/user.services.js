de1aca4c7427d952b1ec98b24d6084da5a9d14a2
Ulkeshwani <sureshwani30@gmail.com>
2022-05-12 13:19:45 +0530

    Created User Services For CRUD Operations

---
+++ src/services/user.services.js
@@ -0,0 +1,20 @@
+import firebase from "../firebase";
+const db = firebase.ref("/users");
+class UserDatabaseService {
+  getAll() {
+    return db;
+  }
+  create(user) {
+    return db.push(user);
+  }
+  update(key, value) {
+    return db.child(key).update(value);
+  }
+  delete(key) {
+    return db.child(key).remove();
+  }
+  deleteAll() {
+    return db.remove();
+  }
+}
+export default new UserDatabaseService();
